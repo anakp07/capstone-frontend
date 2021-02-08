b3e6e21d0ed0341a322da0616ec048f44297b98e
Alice D <alicedilanchian@gmail.com>
2021-02-08 15:05:02 -0500

    added the listview component with a terniary statement, so that it renders if form fields have been updated; ie location has been selected and submited

--- frontend/src/components/LocationSearch.js
+++ frontend/src/components/LocationSearch.js
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
 import LocationSearchForm from './LocationSearchForm';
+import ListView from './ListView';
 
 const LocationSearch = (props) => {
-    return(
+    
+  const isSearchComplete = (formFields) => {
+    if(formFields.country === '' || formFields.state === '' || formFields.city === ''){
+      return false
+    }
+    else{
+      return true
+    }
+  }
+
+  return(
       <div>
         <LocationSearchForm formFields={props.formFields} setFormFields={props.setFormFields}/> 
         {/* <LocationSearchForm isLocationSubmitted={props.isLocationSubmitted} setLocation={props.setLocation} formFields={props.formFields} setFormFields={props.setFormFields}/>  */}
-
+        {isSearchComplete(props.formFields) && <ListView formFields={props.formFields}/> }
       </div>   
     )
 };
