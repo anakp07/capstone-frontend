48e6d05e3bbd51c095e365ffa868cb4ed43e0937
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2021-02-09 21:36:05 -0800

    updated the Mapview component to display map location

--- frontend/src/components/MapView.js
+++ frontend/src/components/MapView.js
@@ -1,7 +1,28 @@
 import React from 'react';
-const MapView = () => {
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+const mapStyles = {
+    width: '100%',
+    height: '100%',
+  };
+
+const MapView = (props) => {
+
     return (
-        <h3>MapView page</h3>
+        <div> 
+            <h3>MapView page</h3>
+            <Map
+                google={props.google}
+                zoom={8}
+                style={mapStyles}
+                initialCenter={{ lat: 47.444, lng: -122.176}}
+                />
+        </div>
         );
     };
-export default MapView;
+
+    export default GoogleApiWrapper({
+        apiKey: 'AIzaSyBv3w3sBv8bZQac_QyYWfcBx0rQmcVvk-U'
+      })(MapView);
+
+// export default MapView;
