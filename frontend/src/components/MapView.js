8edf40831c0ec6c2917cb040344165f246cad2ae
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2021-02-09 21:57:03 -0800

    updated the MapView file to render mapviews for specific location, added a new Marker.jsx files to create a map marker and added css file to match

--- frontend/src/components/MapView.js
+++ frontend/src/components/MapView.js
@@ -1,28 +1,59 @@
-import React from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+// import React from 'react';
+// import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-const mapStyles = {
-    width: '100%',
-    height: '100%',
-  };
+// const mapStyles = {
+//     width: '100%',
+//     height: '100%',
+//   };
 
-const MapView = (props) => {
+// const MapView = (props) => {
+
+//     return (
+//         <div> 
+//             <h3>MapView page</h3>
+//             <Map
+//                 google={props.google}
+//                 zoom={8}
+//                 style={mapStyles}
+//                 initialCenter={{ lat: 47.444, lng: -122.176}}
+//                 />
+//         </div>
+//         );
+//     };
+
+//     export default GoogleApiWrapper({
+//         apiKey: 'AIzaSyBv3w3sBv8bZQac_QyYWfcBx0rQmcVvk-U'
+//       })(MapView);
+
+// // export default MapView;
+
+
+import React, { useState } from 'react';
+import GoogleMapReact from 'google-map-react';
+import Marker from './Marker';
+
+
+
+const MapView = (props: any) => {
+    const [center, setCenter] = useState({lat: 47.444, lng: -122.176 });
+    const [zoom, setZoom] = useState(11);
 
     return (
-        <div> 
-            <h3>MapView page</h3>
-            <Map
-                google={props.google}
-                zoom={8}
-                style={mapStyles}
-                initialCenter={{ lat: 47.444, lng: -122.176}}
-                />
+        <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+            bootstrapURLKeys={{ apiKey: 'AIzaSyBv3w3sBv8bZQac_QyYWfcBx0rQmcVvk-U' }}
+            defaultCenter={center}
+            defaultZoom={zoom}
+            >
+            <Marker
+                lat={47.444}
+                lng={-122.176}
+                name="My Marker"
+                color="blue"
+            />
+        </GoogleMapReact>
         </div>
-        );
-    };
+    );
+}
 
-    export default GoogleApiWrapper({
-        apiKey: 'AIzaSyBv3w3sBv8bZQac_QyYWfcBx0rQmcVvk-U'
-      })(MapView);
-
-// export default MapView;
+export default MapView;
