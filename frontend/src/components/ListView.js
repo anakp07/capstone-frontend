bb0bfc436d6969847b434eca5c1b8b5f3c312a1a
Alice D <alicedilanchian@gmail.com>
2021-02-10 11:20:38 -0500

    renamed landmarkPhoto to unusedPhoto for possible future uses; currently that component is not needed nor used anymore

--- frontend/src/components/ListView.js
+++ frontend/src/components/ListView.js
@@ -1,7 +1,7 @@
 // import React from 'react';
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
-import Photo from './LandmarkPhoto';
+import Photo from './unusedPhoto';
 import PropTypes from 'prop-types';
 
 const ListView = (props) => {
