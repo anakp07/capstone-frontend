d87cef51b10753096fed132403ebbe05061308e6
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2021-02-15 19:27:23 -0800

    updated AddPicture.js to track photoUrl using UseState

--- frontend/src/components/AddPicture.js
+++ frontend/src/components/AddPicture.js
@@ -4,7 +4,6 @@ import S3FileUpload from 'react-s3';
 
 const AddPicture = () => {
 
-
 const config = {
   accessKeyId: process.env.REACT_APP_AWS_ACCESS_ID,
   secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACESS_KEY,
@@ -12,14 +11,17 @@ const config = {
   region: process.env.REACT_APP_REGION
 }
 
+const [photoUrl, setPhotoUrl] = useState("")
+
 const upload=(e) => {
   S3FileUpload.uploadFile(e.target.files[0],config)
   .then((data) => {
     console.log(data.location)
+    setPhotoUrl(data.location)
   })
   .catch ((error) => {
     console.log({error})
-    // alert(error)
+    alert(error)
   })
 }
 
@@ -28,7 +30,7 @@ const upload=(e) => {
       <div>
         <h3> Upload your Photo</h3>
         <input type="file" onChange={upload}/>
-        <AddPictureForm/> 
+        { photoUrl ? <AddPictureForm photoUrl={photoUrl} /> :"Upload Photo" } 
       </div>   
     )
 };
