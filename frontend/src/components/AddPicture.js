136f451a6db02deb3102ac4e53cc3110c9f539cc
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2021-02-15 17:27:50 -0800

    created function to allow users insde AddPicture.js to upload files directly to our AWS S3 bucket

--- frontend/src/components/AddPicture.js
+++ frontend/src/components/AddPicture.js
@@ -1,9 +1,33 @@
 import React, { useState } from 'react';
 import AddPictureForm from './AddPictureForm';
+import S3FileUpload from 'react-s3';
 
 const AddPicture = () => {
+
+
+const config = {
+  accessKeyId: process.env.REACT_APP_AWS_ACCESS_ID,
+  secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACESS_KEY,
+  bucketName: process.env.REACT_APP_BUCKET_NAME,
+  region: process.env.REACT_APP_REGION
+}
+
+const upload=(e) => {
+  S3FileUpload.uploadFile(e.target.files[0],config)
+  .then((data) => {
+    console.log(data.location)
+  })
+  .catch ((error) => {
+    console.log({error})
+    // alert(error)
+  })
+}
+
+
     return(
       <div>
+        <h3> Upload your Photo</h3>
+        <input type="file" onChange={upload}/>
         <AddPictureForm/> 
       </div>   
     )
